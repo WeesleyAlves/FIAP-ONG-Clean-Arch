c0db9bdfdcb6b2cb8d499bb95bfaa5148d09e19e
Wesley Alves <weealves1@gmail.com>
2025-06-27 14:25:50 -0300

    refactor/pacotes-recebidos: adiciona gateway base

---
+++ src/Application/Gateways/PacoteGateway.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Src\Application\Gateways;
+
+use PDO;
+use Src\Core\Pacote\Entities\PacoteRecebidoEntity;
+use Src\Core\Pacote\Interfaces\PacoteDataSource;
+
+
+final class PacoteGateway implements PacoteDataSource
+{
+    private PDO $conn;
+
+    public function __construct(PDO $conn) {
+        $this->conn = $conn;
+    }
+
+    public function savePacoteRecebido(PacoteRecebidoEntity $pacoteEntity): PacoteRecebidoEntity{
+        // TODA A LOGICA DE INSERÇÃO
+        // $this->conn->exec("FAZ ALGUMA COISA AI");
+
+        //SETA O ID DO NEGOCIO E RETORNA;
+        $pacoteEntity->setId(1);
+        return $pacoteEntity;
+    }
+}
