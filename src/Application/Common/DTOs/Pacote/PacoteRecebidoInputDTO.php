56eab95bae79fa6622fe86279b7488fd55c34026
Wesley Alves <weealves1@gmail.com>
2025-06-27 13:37:15 -0300

    refactor/pacotes-recebidos: adiciona mapper no DTO de entrada

--- src/Application/Common/DTOs/Pacote/PacoteRecebidoInputDTO.php
+++ src/Application/Common/DTOs/Pacote/PacoteRecebidoInputDTO.php
@@ -2,20 +2,37 @@
 
 namespace Src\Application\Common\DTOs\Pacote;
 
+use InvalidArgumentException;
+
 final class PacoteRecebidoInputDTO{
     public string $dataRecebimento;
     public string $doador;
 
     public function __construct(string $dataRecebimento, string $doador) {
         if (empty($dataRecebimento)) {
-            throw new \InvalidArgumentException('Data de recebimento não pode ser vazia.');
+            throw new InvalidArgumentException('Data de recebimento não pode ser vazia.');
         }
 
         if (empty($doador)) {
-            throw new \InvalidArgumentException('Doador não pode ser vazio.');
+            throw new InvalidArgumentException('Doador não pode ser vazio.');
         }
 
         $this->dataRecebimento = $dataRecebimento;
         $this->doador = $doador;
+    }
+
+    public static function fromArray( array $data ): self{
+        if (!isset($data['dataRecebimento']) || !is_string($data['dataRecebimento'])) {
+            throw new InvalidArgumentException('Campo "dataRecebimento" é obrigatório e deve ser uma string.');
+        }
+
+        if (!isset($data['doador']) || !is_string($data['doador'])) {
+            throw new InvalidArgumentException('Campo "doador" é obrigatório e deve ser uma string.');
+        }
+
+        return new self(
+            $data['dataRecebimento'],
+            $data['doador']
+        );
     }
 }
