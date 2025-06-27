3b349e2fc85667565d254172f47d9a2443feffee
Wesley Alves <weealves1@gmail.com>
2025-06-27 13:29:25 -0300

    feat/pacote-recebido: adiciona useCase

---
+++ src/Core/Pacote/UseCases/CriarPacoteRecebido.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace Src\Core\Pacote\UseCases;
+
+use Exception;
+use Src\Application\Common\DTOs\Pacote\PacoteRecebidoInputDTO;
+use Src\Core\Pacote\Entities\PacoteRecebidoEntity;
+use Src\Core\Pacote\Interfaces\PacoteDataSource;
+
+final class CriarPacoteRecebido{
+    private PacoteDataSource $pacoteDataSource;
+
+    public function __construct(PacoteDataSource $pacoteDataSource) {
+        $this->pacoteDataSource = $pacoteDataSource;
+    }
+
+    public function execute( PacoteRecebidoInputDTO $pacoteRecebidoDto ): PacoteRecebidoEntity{
+        try {
+            $pacoteEntity = PacoteRecebidoEntity::create(
+                $pacoteRecebidoDto->dataRecebimento,
+                $pacoteRecebidoDto->doador
+            );
+
+        } catch (Exception $e) {
+            throw new Exception("Erro ao criar entidade de Pacote: " . $e->getMessage() );
+        }
+
+        $this->pacoteDataSource->savePacoteRecebido($pacoteEntity);
+
+        return $pacoteEntity;
+    }
+}
